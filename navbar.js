bff8c80a8a8d7bac3a9e08950543a9220f6ede38
Lunar-Academy <69018644+Lunar-Academy@users.noreply.github.com>
2024-09-11 22:28:36 -0400

    added nav and footer includes, need to update for all pages

---
+++ navbar.js
@@ -0,0 +1,21 @@
+document.write(`
+
+    		<nav id="menu">
+			<div class="inner">
+				<h2>Menu</h2>
+				<ul class="links">
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/#one">Mission</a></li>
+                    <li><a href="/#two">Programs</a></li>
+                    <li><a href="about">About Us</a></li>
+                    <li><a href="/#four">Accomplishments</a></li>
+                    <li><a href="sign-up">Sign Up</a></li>
+					<li><a href="online-courses">Online Class Information</a></li>
+					<li><a href="#footer">Contact Us</a></li>
+					<li><a href="donate">Donate</a></li>
+				</ul>
+				<a href="#" class="close">Close</a>
+			</div>
+		</nav>
+
+    `);
